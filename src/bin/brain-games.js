14dd0b2866f4279cfe03f304717bebda7ec9e409
neihaoo <neihaoo@gmail.com>
2019-08-31 17:12:13 +0300

    add greeting

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+import readlineSync from '..';
+
 console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
+
+console.log('Hello ' + userName + '!');
